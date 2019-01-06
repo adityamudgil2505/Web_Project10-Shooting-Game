34b7c9ba31e44ee4dbffc173781d3e8a9eb9658e
Aditya Mudgil <adityamudgil2505@gmail.com>
2019-01-06 22:29:37 +0530

    Initialize HTML & CSS
    JS is not done

---
+++ script/main.js
@@ -0,0 +1,13 @@
+var cartoon = $(".combo > div:first-child");
+
+var speed;
+function randomGen(min, max){
+  return Math.random()*(max-min) +min;
+}
+//setInterval(_ => console.log(randomGen(2,10)), 1000);
+//console.log(randomGen(2,10));
+
+// popup Cartoon
+// function()
+// { 
+// }
